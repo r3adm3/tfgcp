29ae398ece81860f3253fd82dbda67c12438229f
Adrian Farnell <adrianjfarnell@gmail.com>
2019-08-15 07:52:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Hookup terraform to GCP
+
+cite: https://mrooding.me/using-terraform-to-bootstrap-a-google-cloud-platform-cluster-fb709d13f6f9
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "gcloud-region"    { default = "europe-west1" }
+variable "gcloud-zone"      { default = "europe-west1-b" }
+variable "gcloud-project"   { default = "sample-project" }
+variable "platform-name"    { default = "sample-platform"  }
